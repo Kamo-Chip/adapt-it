a5624032f1aacbe3b6f43cd329d4d04d0f39dd20
Muaaz Bayat <muaazbayat@gmail.com>
2024-09-05 13:57:05 +0200

    clerk and some boiler

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,15 @@
-import NavBar from "@/components/nav-bar";
 import "./globals.css";
-
+import NavBar from "../components/nav-bar"
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      {
+        
+      }
       <body className="">
-        <NavBar />
+      <NavBar>
+          
+          </NavBar>
         {children}
       </body>
     </html>
